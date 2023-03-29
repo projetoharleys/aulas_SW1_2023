be9bc7f07e30fa8660759fb773f5ff7b07b7a594
Harley <100374099+projetoharleys@users.noreply.github.com>
2023-03-29 14:31:59 -0300

    Add files via upload

---
+++ métodos_assessores/Produto.cs
@@ -0,0 +1,17 @@
+namespace exemplo;
+class Produto
+{
+    private string? Nome {get;set;}
+    private double Preco {get;set;}
+    
+    public void MostraDados(){
+        Console.WriteLine("Nome: " + this.Nome);
+        Console.WriteLine("Preço: " + this.Preco);
+    }
+    public void AlteraNome(string nome){
+        this.Nome = nome;
+    }
+    public void AlteraPreco(double preco){
+        this.Preco = preco;
+    }
+}
